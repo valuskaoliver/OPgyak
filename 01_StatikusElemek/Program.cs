6d4f2c1a5bae7ebb4d6fc84b89cfd02b2d27da8a
valuskaoliver <valuska.oliver.2023d@telekimezotur.hu>
2026-09-24 10:46:56 +0200

    1.feladat

---
+++ 01_StatikusElemek/Program.cs
@@ -0,0 +1,5 @@
+﻿using _01_StatikusElemek;
+
+Console.WriteLine($"100Eur to Huf: {ValutaValto.EurToHuf(100)}");
+Console.WriteLine($"250Usd to Huf: {ValutaValto.UsdToHuf(250)}");
+Console.WriteLine($"Total conversions: {ValutaValto.GetOsszesAtvaltas()}");
---
+++ 1.projekt/ValutaValto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1.projekt
+{
+    public static class ValutaValto
+    {
+    }
+}
