6f5965494fb99138c2571938f483a7b553f822eb
valuskaoliver <valuska.oliver.2023d@telekimezotur.hu>
2026-09-24 11:37:53 +0200

    2.Feladat

---
+++ 03_FeludefinialasEsListak/Program.cs
@@ -0,0 +1,9 @@
+﻿using _03_FeludefinialasEsListak;
+
+Jatekos Bela = new Jatekos("Bela", 5, 100);
+//Console.WriteLine($"Játékos: {Bela.Nev} Szint: {Bela.Szint} Pontszám: {Bela.Pontszam}");
+Console.WriteLine(Bela.ToString());
+
+Jatekos Eva = new Jatekos("Eva", 120, 200);
+//Console.WriteLine($"Játékos: {Eva.Nev} Szint: {Eva.Szint} Pontszám: {Eva.Pontszam}");
+Console.WriteLine(Eva.ToString());
