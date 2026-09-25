e44e0f971ba4d9d7085de590b828b17b79f7ce6b
valuskaoliver <valuska.oliver.2023d@telekimezotur.hu>
2026-09-25 10:45:53 +0200

    4.feladat

---
+++ 02_AdatvedelemEsKonstruktor/Bankszamla.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02_AdatvedelemEsKonstruktor
+{
+    internal class Bankszamla
+    {
+        private string szamlaszam;
+        private double egyenleg;
+        private double hitelKeret;
+
+        public string Szamlaszam
+        {
+            get { return szamlaszam; }
+        }
+        public double Egyenleg
+        {
+            get { return egyenleg; }
+        }
+        public double HitelKeret
+        {
+            get { return hitelKeret; }
+            set
+            {
+                if (value < 0)
+                {
+                    hitelKeret = 0;
+                }
+                else
+                {
+                    hitelKeret = value;
+                }
+            }
+
+        }
+
+        public Bankszamla(string beszamlaszam)
+        {
+            szamlaszam = beszamlaszam;
+            egyenleg = 0;
+            hitelKeret = 50000;
+        }
+        public Bankszamla(string beszamlaszam, double induloegyenleg, double behitelKeret)
+        {
+            szamlaszam = beszamlaszam;
+            egyenleg = induloegyenleg;
+            hitelKeret = behitelKeret;
+        }
+        public void Befizetes(double osszeg)
+        {
+            if (osszeg > 0)
+            {
+                    egyenleg += osszeg;
+            }
+        }
+        public bool Kivetel(double osszeg)
+        {
+            if (osszeg > 0)
+            {
+                if ( egyenleg-osszeg >= -(hitelKeret))
+                {
+                    egyenleg -= osszeg;
+                    return true;
+                }
+    
+            }
+            return false;
+        }
+
+    }
+}
+
---
+++ 02_AdatvedelemEsKonstruktor/Program.cs
@@ -0,0 +1,10 @@
+﻿using _02_AdatvedelemEsKonstruktor;
+
+Bankszamla Anna = new Bankszamla("100");
+Bankszamla Bela = new Bankszamla("200", 100000, 50000);
+
+Anna.Befizetes(10000);
+Bela.Kivetel(20000);
+
+Console.WriteLine($"Anna számlaszáma: {Anna.Szamlaszam}, egyenlege: {Anna.Egyenleg}, hitelkerete: {Anna.HitelKeret}");
+Console.WriteLine($"Béla számlaszáma: {Bela.Szamlaszam}, egyenlege: {Bela.Egyenleg}, hitelkerete: {Bela.HitelKeret}");
